4ec122c31ef642b4f153166d85fb6e597b24b7a0
BasedMemes <a80095202@gmail.com>
2024-06-10 16:39:30 -0400

    Add files via upload

---
+++ js/tv.js
@@ -0,0 +1,218 @@
+const videos = [
+  {
+    'code': 'FkCxR3Iw4ng',
+    'title': 'The Forgotten Art Of Windows Joke Applications',
+    'description': ''
+  },
+  {
+    'code': 'Ke_9ethHTG4',
+    'title': 'we need jungle',
+    'description': 'can\'t take drum & bass'
+  },
+  {
+    'code': 'ohk8Y0pxbcU',
+    'title': 'the dangers of marijuana',
+    'description': ''
+  },
+  {
+    'code': '9d5zlmhfhXQ',
+    'title': 'People who like the Beatles',
+    'description': ''
+  },
+  {
+    'code': 'KIXTNumrDc4',
+    'title': 'Tayne',
+    'description': ''
+  },
+  {
+    'code': 'ipYGcrBo-uI',
+    'title': 'In your face California!',
+    'description': ''
+  },
+  {
+    'code': 'txztnBLVvIA',
+    'title': 'Vowel Movements by The Evens',
+    'description': ''
+  },
+  {
+    'code': 'bPqRpar4PyE',
+    'title': 'Darkplace - Good Gravy!',
+    'description': ''
+  },
+  {
+    'code': '6-1Ue0FFrHY',
+    'title': 'Long Long Man',
+    'description': ''
+  },
+  {
+    'code': 'apVR5Htz0K4',
+    'title': 'Useless Box with Surprises',
+    'description': ''
+  },
+  {
+    'code': 'LZgeIReY04c',
+    'title': 'Never Illegally Download',
+    'description': ''
+  },
+  {
+    'code': 'nhVJhRhJbJE',
+    'title': 'That Mitchell and Webb Look - Mundane Property Shows',
+    'description': ''
+  },
+  {
+    'code': 'SAP-b7E4L6k',
+    'title': 'Boomhauer\'s Advice on Holding Grudges',
+    'description': ''
+  },
+  {
+    'code': 'qRgLbSnFKy0',
+    'title': 'Nude Batter Tetsuo by Takashi Taniguchi (ヌードバッター鉄雄 English Subtitles)',
+    'description': ''
+  },
+  {
+    'code': 'Cm65-ozqk1k',
+    'title': 'X-Men 90s Cartoon Theme Song with Lyrics',
+    'description': ''
+  },
+  {
+    'code': '2e5tAlelJXc',
+    'title': 'ok, throw me some numbers',
+    'description': ''
+  },
+  {
+    'code': 'ThtqoNKTsQ',
+    'title': 'The DVD Logo Is Wrong',
+    'description': ''
+  },
+  {
+    'code': 'FAiyGC13oVc',
+    'title': 'Help! I need a sandwich!',
+    'description': ''
+  },
+  {
+    'code': 'hVJtPuuQK_s',
+    'title': 'The Kiffness x Ognjen & Sinisa - Insomnia (Balkan Club Remix)',
+    'description': ''
+  },
+  {
+    'code': 'g3CyRZY90M4',
+    'title': 'Why Most Japanese Clip Art Looks Like This',
+    'description': ''
+  },
+  {
+    'code': 'MkLnwFnpvlU',
+    'title': 'Irregular Verbs Grammar Rap Song',
+    'description': ''
+  },
+  {
+    'code': 'Nx-x_1lIXh4',
+    'title': 'Holding Out for a Hero - Hildegard von Blingin’ & Whitney Avalon (Bardcore | Medieval Style)',
+    'description': ''
+  },
+  {
+    'code': 'yGS2WTgrGAg',
+    'title': 'Recycle Man',
+    'description': ''
+  },
+  {
+    'code': 'GTwrVAbV56o',
+    'title': 'Flex Mex',
+    'description': ''
+  },
+  {
+    'code': 'qpUXCVqHAWw',
+    'title': 'spiderman\'s true identity',
+    'description': ''
+  },
+  {
+    'code': 'SjYHtvIPdv8',
+    'title': 'Ghost Mustard',
+    'description': ''
+  },
+  {
+    'code': 'bGROryzHLII',
+    'title': 'Rublight: The Light You Can Rub On Anything',
+    'description': ''
+  },
+  {
+    'code': 'pLHHQ270w0',
+    'title': 'Broad City - Drum Battle',
+    'description': 'the best cold open to an episode, ever'
+  },
+  {
+    'code': '4eJJF11bIxM',
+    'title': '¡Ayúdame!',
+    'description': 'cuando tienes muchos quehaceres'
+  },
+  {
+    'code': 'lj10_Br86iI',
+    'title': 'Louis Cole Plays Some Drum N\' Bass',
+    'description': ''
+  },
+  {
+    'code': 'XlkoSULRJxY',
+    'title': 'Henry Rollins Chats With Guitarist Steve Vai',
+    'description': ''
+  },
+  {
+    'code': 'v4IC7qaNr7I',
+    'title': 'KXVO Pumpkin Dance',
+    'description': 'the only halloween content i care about'
+  },
+  {
+    'code': 'BZlRt05RY9Y',
+    'title': 'Star Trek Acid Party',
+    'description': 'i don\'t even know. i actually watched this whole thing and it was surprisingly good, lots of great music used'
+  },
+  {
+    'code': 'n0XaSvhTYd4',
+    'title': 'i\'m a piece of garbage',
+    'description': ''
+  },
+  {
+    'code': 'wbfu39l0kxg',
+    'title': 'Earn $20K EVERY MONTH by being your own boss',
+    'description': 'some comedic eldritch horror from brian david gilbert'
+  },
+  {
+    'code': 'iVzGdLDBGas',
+    'title': 'NPR\'s Delicious Dish - BBQ',
+    'description': 'well i\'ll be darned'
+  },
+  {
+    'code': 'ES14xgxYlI0',
+    'title': 'Bo Hansson - The Black Riders & Flight to the Ford',
+    'description': 'a track from bo hansson\'s lord of the rings-inspired album set to the appropriate scene from the 2003 movie. i kinda want this person to do the rest of the album/movie.'
+  },
+  {
+    'code': 'Kfl3N9nesRg',
+    'title': 'Eminem as a Talking Heads song',
+    'description': 'does what it says on the tin. extremely catchy.'
+  },
+  {
+    'code': 'g-sgw9bPV4A',
+    'title': 'Kazoo Kid - Trap Remix',
+    'description': ''
+  },
+  {
+    'code': '09aji2KeaG8',
+    'title': 'Wewewewewe',
+    'description': ''
+  },
+  {
+    'code': 'ATGbHjUKnqM',
+    'title': 'Star Wars Fan Plays Imperial March on Coffee Stirrer',
+    'description': ''
+  },
+  {
+    'code': 'UWWLwtMLWCI',
+    'title': 'Hey Cat. Hey',
+    'description': 'heeeEEEYYY'
+  },
+  {
+    'code': 'ZfVci0LazqI',
+    'title': 'Rodrigo y Gabriela - Take Five - Dave Brubeck Cover',
+    'description': 'apart from their greatness, i love the bit at the end'
+  },
+];
+
